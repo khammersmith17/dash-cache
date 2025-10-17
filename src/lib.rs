ea80226f86d1d452543ec9a2668c91c9ba599b81
khammersmith17 <kilianhammersmith@gmail.com>
2025-10-16 21:31:09 -0400

    some minor updates

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use std::hash::Hash;
 use std::rc::{Rc, Weak};
 
+#[derive(Debug)]
 struct ListNode<K, T> {
     value: T,
     key: K,
@@ -10,9 +11,9 @@ struct ListNode<K, T> {
     next: Option<Weak<RefCell<ListNode<K, T>>>>,
 }
 
+#[derive(Debug)]
 pub struct LruCache<K, T> {
     cap: usize,
-    len: usize,
     node_map: HashMap<K, Rc<RefCell<ListNode<K, T>>>>,
     head: Option<Weak<RefCell<ListNode<K, T>>>>,
     tail: Option<Weak<RefCell<ListNode<K, T>>>>,
@@ -27,19 +28,22 @@ where
         let node_map: HashMap<K, Rc<RefCell<ListNode<K, T>>>> = HashMap::with_capacity(cap);
         LruCache {
             cap,
-            len: 0_usize,
             node_map,
             head: None,
             tail: None,
         }
     }
 
+    pub fn contains(&self, key: &K) -> bool {
+        self.node_map.contains_key(key)
+    }
+
     fn is_empty(&self) -> bool {
-        self.len == 0
+        self.node_map.len() == 0
     }
 
     fn is_full(&self) -> bool {
-        self.len == self.cap
+        self.node_map.len() == self.cap
     }
 
     pub fn insert(&mut self, key: K, value: T) {
@@ -64,54 +68,62 @@ where
 
         if self.is_full() {
             self.pop_tail()
-        } else {
-            self.len += 1
         }
 
-        let new_weak_head = Rc::downgrade(&new_node);
-
-        self.push_node_to_head(new_weak_head);
+        self.push_node_to_head(Rc::downgrade(&new_node));
         self.node_map.insert(key, new_node);
     }
 
     fn unlink_node(&mut self, node: Rc<RefCell<ListNode<K, T>>>) {
         let node_ref = node.borrow();
-        let Some(prev_weak) = node_ref.prev.clone() else {
-            // node is already head
-            return;
-        };
+        let prev_weak = node_ref.prev.clone();
+        let next_weak = node_ref.next.clone();
 
-        let prev_rc = prev_weak.upgrade().unwrap();
-        let mut prev_ref = prev_rc.borrow_mut();
+        if let Some(ref prev) = prev_weak {
+            if let Some(prev_rc) = prev.upgrade() {
+                let mut prev_ref = prev_rc.borrow_mut();
+                prev_ref.next = next_weak.clone();
+            }
+        }
 
-        let Some(next_weak) = node_ref.next.clone() else {
-            // node has no next neighbor
-            return;
-        };
+        if let Some(ref next) = next_weak {
+            if let Some(next_rc) = next.upgrade() {
+                let mut next_ref = next_rc.borrow_mut();
+                next_ref.prev = prev_weak.clone();
+            }
+        }
 
-        let next_rc = next_weak.upgrade().unwrap();
-        let mut next_ref = next_rc.borrow_mut();
+        if prev_weak.is_none() {
+            self.head = next_weak.clone();
+        }
 
-        prev_ref.next = Some(next_weak);
-        next_ref.prev = Some(prev_weak);
+        if next_weak.is_none() {
+            self.tail = prev_weak.clone();
+        }
     }
 
     fn push_node_to_head(&mut self, node: Weak<RefCell<ListNode<K, T>>>) {
         if self.is_empty() {
-            self.head = Some(node);
+            self.head = Some(node.clone());
+            self.tail = Some(node);
             return;
         }
 
-        let curr_head = self.head.clone().unwrap();
-        let curr_head_rc = curr_head.upgrade().unwrap();
-        let mut curr_head_mut: RefMut<ListNode<K, T>> = curr_head_rc.as_ref().borrow_mut();
-        curr_head_mut.prev = Some(node.clone());
-        drop(curr_head_mut);
+        let old_head = self.head.clone();
 
-        let new_head_rc = node.clone().upgrade().unwrap();
-        let mut new_head_mut: RefMut<ListNode<K, T>> = new_head_rc.as_ref().borrow_mut();
-        new_head_mut.prev = None;
-        new_head_mut.next = self.head.clone();
+        if let Some(curr_head) = old_head {
+            if let Some(curr_head_rc) = curr_head.upgrade() {
+                let mut curr_head_mut = curr_head_rc.as_ref().borrow_mut();
+                curr_head_mut.prev = Some(node.clone());
+            }
+        }
+
+        if let Some(new_head_rc) = node.upgrade() {
+            let mut new_head_mut = new_head_rc.as_ref().borrow_mut();
+            new_head_mut.prev = None;
+            new_head_mut.next = self.head.clone();
+        }
+
         self.head = Some(node)
     }
 
@@ -125,13 +137,19 @@ where
         let curr_tail_ref: Ref<ListNode<K, T>> = curr_tail_rc.as_ref().borrow();
 
         let key_to_pop = curr_tail_ref.key.clone();
+        if let Some(new_tail) = curr_tail_ref.prev.clone() {
+            let new_tail_rc = new_tail.upgrade().unwrap();
+            let mut new_tail_ref = new_tail_rc.borrow_mut();
+            new_tail_ref.next = None;
+            drop(new_tail_ref);
+
+            self.tail = Some(new_tail)
+        } else {
+            self.head = None;
+            self.tail = None;
+        }
+
         self.node_map.remove(&key_to_pop);
-        let new_tail = curr_tail_ref.prev.clone().unwrap();
-        let new_tail_rc = new_tail.upgrade().unwrap();
-        let mut new_tail_ref = new_tail_rc.borrow_mut();
-        new_tail_ref.next = None;
-        drop(new_tail_ref);
-        self.tail = Some(new_tail)
     }
 
     pub fn get(&mut self, key: &K) -> Option<T> {
